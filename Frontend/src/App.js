b306be7edd15429393bc35179f3d809b4f999d04
Thanh Tuấn <tuanndt76@outlook.com>
2024-04-19 15:29:47 +0700

    feat: Create basic Frontend

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <code>Learning and Creating MERN - Simple Blog</code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
