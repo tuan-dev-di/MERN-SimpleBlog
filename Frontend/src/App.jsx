b784809f1b6cfcfc6f67c019f817fa13023634a4
Thanh Tuấn <tuanndt76@outlook.com>
2024-04-21 19:46:53 +0700

    feat: Create new pages and routes - Install React Router Dom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Project from "./pages/Project"
 
 export default function App() {
   return (
-    <div className='text-3xl text-teal-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/sign-in" element={<SignIn/>}></Route>
+        <Route path="/sign-up" element={<SignUp/>}></Route>
+        <Route path="/project" element={<Project/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
