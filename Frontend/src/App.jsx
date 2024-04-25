fd13ae459270a4b9a2cd6d367d1c514e616bd088
Thanh Tuấn <tuanndt76@outlook.com>
2024-04-25 21:31:09 +0700

    feat: Create a Header bar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import Dashboard from "./pages/Dashboard"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Project from "./pages/Project"
+import {Header} from "./components/index"
+import {Home, Dashboard, About, SignIn, SignUp, Project} from "./pages/index"
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,57 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa6";
+
+const Header = () => {
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl dark:text-white font-semibold"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-emerald-400 to-cyan-400 rounded-md">
+          ArysDomi's
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Searching..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        ></TextInput>
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch className="w-6 h-5"></AiOutlineSearch>
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon></FaMoon>
+        </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="greenToBlue" pill>
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/project"} as={"div"}>
+          <Link to="/project">Project</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
