2de023cff7fe8c7c8303e6ebad222ebbf9b70699
Thanh Tuấn <tuanndt76@outlook.com>
2024-05-07 12:49:42 +0700

    feat: Connected to MongoDB with 2 options

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,40 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+/**
+ * * Connect with PORT
+ */
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is on port ${PORT}!`));
+
+/** ----------------------------------------------------------------------------------------------
+ * * Connect to Mongoose using with Username, Password, Cluster, URL by .env
+ */
+const MongoDB_Username = process.env.MongoDB_Username;
+const MongoDB_Password = process.env.MongoDB_Password;
+const MongoDB_Cluster = process.env.MongoDB_Cluster;
+const MongoDB_URL = `mongodb+srv://${MongoDB_Username}:${MongoDB_Password}@${MongoDB_Cluster}.mongodb.net/`;
+
+// const connect_MongoDB = async () => {
+//   try {
+//     await mongoose.connect(MongoDB_URL);
+//     console.log("Mongoose Connected");
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+// connect_MongoDB();
+
+mongoose
+  .connect(MongoDB_URL)
+  .then(() => {
+    console.log("Mongoose Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
